eec00f59295f5ac0b65d6c66b974d038cc82bb38
craftsmon <craftsmon@outlook.com>
2018-06-13 16:03:51 +0300

    Made images, links and content dynamic

--- resources/views/partials/might-like.blade.php
+++ resources/views/partials/might-like.blade.php
@@ -1,27 +1,14 @@
 <div class="might-like-section">
-    <div class="container">
-        <h2>You might also like...</h2>
-        <div class="might-like-grid">
-            <a href="#" class="might-like-product">
-                <img src="{{ asset('img/macbook-pro.png') }}" alt="product">
-                <div class="might-like-product-name">MacBook Pro</div>
-                <div class="might-like-product-price">$2499.99</div>
-            </a>
-            <a href="#" class="might-like-product">
-                <img src="{{ asset('img/macbook-pro.png') }}" alt="product">
-                <div class="might-like-product-name">MacBook Pro</div>
-                <div class="might-like-product-price">$2499.99</div>
-            </a>
-            <a href="#" class="might-like-product">
-                <img src="{{ asset('img/macbook-pro.png') }}" alt="product">
-                <div class="might-like-product-name">MacBook Pro</div>
-                <div class="might-like-product-price">$2499.99</div>
-            </a>
-            <a href="#" class="might-like-product">
-                <img src="{{ asset('img/macbook-pro.png') }}" alt="product">
-                <div class="might-like-product-name">MacBook Pro</div>
-                <div class="might-like-product-price">$2499.99</div>
-            </a>
-        </div>
+  <div class="container">
+    <h2>You might also like...</h2>
+    <div class="might-like-grid">
+      @foreach ($mightAlsoLike as $product)
+        <a href="{{ route('shop.show', $product->slug) }}" class="might-like-product">
+          <img src="{{ asset('img/macbook-pro.png') }}" alt="product">
+          <div class="might-like-product-name">{{ $product->name }}</div>
+          <div class="might-like-product-price">{{ $product->presetPrice() }}</div>
+        </a>
+      @endforeach
     </div>
+  </div>
 </div>
