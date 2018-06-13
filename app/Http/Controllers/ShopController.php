4f52aabe96d0554ea4ccf588919a4066bd275e1d
craftsmon <craftsmon@outlook.com>
2018-06-13 15:59:47 +0300

    Controller for rendering shop items

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        $products = Product::inRandomOrder()->take(10)->get();
+
+        return view('shop')->with('products', $products);
+    }
+
+    /**
+     * Display the specified resource
+     * 
+     * @param string $slug
+     * @return \Illuminate\Http\Response
+     */
+    public function show ($slug)
+    {
+        $product = Product::where('slug', $slug)->firstOrFail();
+        $mightAlsoLike = Product::where('slug', '!=', $slug)->inRandomOrder()->take(4)->get();
+
+        return view('product')->with([
+            'product' => $product,
+            'mightAlsoLike' => $mightAlsoLike,
+        ]);
+    }
+}
